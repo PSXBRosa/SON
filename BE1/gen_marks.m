bc76143f5a8b371bc05b0e4a68e8a02879d74e49
Pedro Rosa <pedro_sxbr@usp.br>
2023-12-11 16:20:38 +0100

    Fixed gen_marks.m
    gen_marks.m is a file intended to automatically find the f0 inside
    complex audio files. This'll be later used for the PSOLA algorithm.
    Currently the markings are found using the findpeaks function from
    octave, however, in the future a more complex method should be used
    for example, YIN, a fundamental frequency estimator for speech and
    music.

--- BE1/gen_marks.m
+++ BE1/gen_marks.m
@@ -1,17 +1,38 @@
 pkg load signal
 
-arg_list = argv ();
-audio_file_path = arg_list[1];
+# load audio file
+audio_file_path = "effacer.wav";
+text_file_path = "effacer.txt";
+
 [y, fs] = audioread(audio_file_path);
-ts = seconds(0:1/fs:(size(y,1)-1)/fs);
+ts = 0:1/fs:(length(y)*(1/fs))-(1/fs);
 
-[pks idx] = findpeaks(y);
+# load ground truth
+marks = []; % samplestamps
+flags = []; % 1 when silent, 0 otherwise
 
-figure(1);
-clf;
+if text_file_path != " "
+  fileID = fopen(text_file_path);
+
+  content = textscan(fileID, "%u %u");
+  marks = content{1};
+  flags = content{2};
+
+  fclose(text_file_path);
+end;
+
+[pks idx] = findpeaks(y, "DoubleSided");
+
+figure;
 grid on;
 hold on;
-plot(ts, y, t(idx), data(idx), 'or');
-for ii=1:length(marques)
-  plot([marques(ii) marques(ii)],[-1 1],'r');
+samples = 1:length(y);
+plot(samples, y, samples(idx), y(idx), 'or');
+for i=1:length(marks)
+    if (flags(i)==1)
+      plot([marks(i) marks(i)],[-1 1],'r');
+    else
+      plot([marks(i) marks(i)],[-1 1],'k');
+    end;
 end;
+hold off;
